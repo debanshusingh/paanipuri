d2f50686ef649adc607bbd30f1fba58a481347d3
Bradley Crusco <bcrusco627@gmail.com>
2015-04-26 19:13:26 -0400

    Shader clean up and renamed the shaders for the particles.

--- paanipuri/shaders/fs_particle.glsl
+++ paanipuri/shaders/fs_particle.glsl
@@ -1,7 +1,6 @@
 #version 330 core
 
 in vec3 outColor;
-//in vec3 lightDirection;
 
 out vec4 OutColor;
 
@@ -12,12 +11,8 @@ void main()
     
     normal.xy = gl_PointCoord * 2.0 - vec2(1.0); //this will have to be modified to change the radius
     float mag = dot(normal.xy, normal.xy);
-    if(mag > 1.0) discard; //kill any pixels outsize of hte circle
+    if(mag > 1.0) discard; //kill any pixels outsize of the circle
     normal.z = sqrt(1.0 - mag);
     
-    //lighting calculation
-    //float diffuse = max(0.0, dot(lightDirection, normal));
-    
-    //OutColor = vec4(outColor, 1) * diffuse;
     OutColor = vec4(outColor,1.0);
 }
